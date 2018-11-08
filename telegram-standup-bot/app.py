7ee1944d3d2f2a0489ec6ecfddcae14c1b53263b
zhukovgreen <zhukovgreen@ya.ru>
2018-11-08 06:51:50 +0100

    Start implementation of the redis

--- telegram-standup-bot/app.py
+++ telegram-standup-bot/app.py
@@ -1,9 +1,13 @@
 import logging
 import sys
+import yarl
 
 from aiogram import Dispatcher, Bot
 from aiogram.contrib.fsm_storage.memory import (
     MemoryStorage,
+)
+from aiogram.contrib.fsm_storage.redis import (
+    RedisStorage2,
 )
 from envparse import env
 
@@ -15,8 +19,20 @@ LOGGING_LEVEL = env(
     "LOGGING_LEVEL", postprocessor=str.upper
 )
 
+MEMORY_TYPE = env.str("MEMORY_TYPE")
+REDIS_URL = yarl.URL(env.str("CACHE"))
+
+storage = (
+    MemoryStorage()
+    if MEMORY_TYPE == "in_memory"
+    else RedisStorage2(
+        REDIS_URL.host,
+        REDIS_URL.port,
+        db=int(REDIS_URL.path[1:]),
+    )
+)
 bot = Bot(token=BOT_TOKEN, parse_mode="markdown")
-dp = Dispatcher(bot, storage=MemoryStorage())
+dp = Dispatcher(bot, storage=storage)
 
 
 def setup_logger():
